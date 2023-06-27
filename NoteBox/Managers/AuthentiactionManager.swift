66f498cf55da0d9f382f0d05fd97cece4ee7824a
Laryokkk <laryokkk@Laryokkks-MBP.homenet.telecomitalia.it>
2023-06-27 19:18:36 +0200

    mc

--- NoteBox/Managers/AuthentiactionManager.swift
+++ NoteBox/Managers/AuthentiactionManager.swift
@@ -8,50 +8,50 @@
 import Foundation
 import FirebaseAuth
 
-struct AuthModel {
-    let uid: String
-    let email: String?
-    let photoURL: String?
-    
-    init(user: User) {
-        self.uid = user.uid
-        self.email = user.email
-        self.photoURL = user.photoURL?.absoluteString
-    }
-}
+//struct AuthModel {
+//    let uid: String
+//    let email: String?
+//    let photoURL: String?
+//
+//    init(user: User) {
+//        self.uid = user.uid
+//        self.email = user.email
+//        self.photoURL = user.photoURL?.absoluteString
+//    }
+//}
 
 final class AuthenticationManager {
-    
-    static let shared = AuthenticationManager()
-    
-    private init() { }
-    
-    @discardableResult
-    func signUp(email: String, password: String) async throws -> AuthModel {
-        let auth = try await Auth.auth().createUser(withEmail: email, password: password)
-        
-        return AuthModel(user: auth.user)
-    }
-    
-    @discardableResult
-    func signIn(email: String, password: String) async throws -> AuthModel {
-        let auth = try await Auth.auth().signIn(withEmail: email, password: password)
-        
-        return AuthModel(user: auth.user)
-    }
-    
-    // checks locally if there is authenticated user
-    func getAuthenticatedUser() throws -> AuthModel {
-        guard let user = Auth.auth().currentUser else {
-            throw URLError(.badServerResponse)
-        }
-        
-        return AuthModel(user: user)
-    }
-    
-    func signOut() throws {
-        try Auth.auth().signOut()
-    }
+//
+//    static let shared = AuthenticationManager()
+//
+//    private init() { }
+//
+//    @discardableResult
+//    func signUp(email: String, password: String) async throws -> AuthModel {
+//        let auth = try await Auth.auth().createUser(withEmail: email, password: password)
+//
+//        return AuthModel(user: auth.user)
+//    }
+//
+//    @discardableResult
+//    func signIn(email: String, password: String) async throws -> AuthModel {
+//        let auth = try await Auth.auth().signIn(withEmail: email, password: password)
+//
+//        return AuthModel(user: auth.user)
+//    }
+//
+//    // checks locally if there is authenticated user
+//    func getAuthenticatedUser() throws -> AuthModel {
+//        guard let user = Auth.auth().currentUser else {
+//            throw URLError(.badServerResponse)
+//        }
+//
+//        return AuthModel(user: user)
+//    }
+//
+//    func signOut() throws {
+//        try Auth.auth().signOut()
+//    }
     
 //    private func signUp() {
 //        Task {
